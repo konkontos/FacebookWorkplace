5abb90c1f4d8116d6944c3ab56042c6aab0b44bb
Konstantinos Kontos <kkon@handmadeapps.tech>
2017-07-03 20:58:30 +0300

    Initial Commit

---
+++ Facebook Workplace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Facebook Workplace
+//
+//  Created by Konstantinos Kontos on 03/07/2017.
+//  Copyright © 2017 Handmade Apps Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
