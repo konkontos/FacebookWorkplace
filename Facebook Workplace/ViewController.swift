fd7db0e677de2acb5d7754340633874d30b358e7
Konstantinos Kontos <kkon@handmadeapps.tech>
2017-07-05 10:36:36 +0300

    commit

--- Facebook Workplace/ViewController.swift
+++ Facebook Workplace/ViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        if #available(OSX 10.11, *) {
+            webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4"
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        webView.load(URLRequest(url: URL(string: "http://work.facebook.com")!))
     }
 
     override var representedObject: Any? {
